06dbf24988c09cd593bc2f1ae0327e0a3c75de9c
Null <42580530+chenxinye@users.noreply.github.com>
2021-11-25 17:16:39 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     install_requires=["numpy>=1.20.0", "scikit-learn", "pandas", "fABBA", "lightgbm", "request"],
     include_dirs=[numpy.get_include()],
     long_description=long_description,
-    author="Stefan Guettel, Xinye Chen",
+    author="Stefan Güttel, Xinye Chen",
     author_email="stefan.guettel@manchester.ac.uk",
     description="A package linking symbolic representation with sklearn for time series prediction",
     long_description_content_type='text/markdown',
