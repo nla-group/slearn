b2f1016d5bf8e471f76b7dbbdd438f1cf9d79389
Null <42580530+chenxinye@users.noreply.github.com>
2022-01-12 17:36:30 +0000

    Update slearn.py

--- slearn/slearn.py
+++ slearn/slearn.py
@@ -36,6 +36,7 @@ import numpy as np
 import pandas as pd
 import lightgbm as lgb
 from scipy.stats import norm
+from scipy.sparse.linalg import svds
 
 from sklearn.svm import SVC
 # from deepforest import CascadeForestClassifier
@@ -324,6 +325,301 @@ class SAX:
         return strings, hashm, inverse_hashm
     
     
+
+
+# python implementation for aggregation
+def aggregate(data, sorting="2-norm", tol=0.5): # , verbose=1
+    """aggregate the data
+    Parameters
+    ----------
+    data : numpy.ndarray
+        the input that is array-like of shape (n_samples,).
+    sorting : str
+        the sorting method for aggregation, default='2-norm', alternative option: '1-norm' and 'lexi'.
+    tol : float
+        the tolerance to control the aggregation. if the distance between the starting point 
+        of a group and another data point is less than or equal to the tolerance,
+        the point is allocated to that group.  
+    Returns
+    -------
+    labels (numpy.ndarray) : 
+        the group categories of the data after aggregation
+    
+    splist (list) : 
+        the list of the starting points
+    
+    nr_dist (int) :
+        number of pairwise distance calculations
+    """
+
+    splist = list() # store the starting points
+    len_ind = data.shape[0]
+    if sorting == "2-norm": 
+        sort_vals = np.linalg.norm(data, ord=2, axis=1)
+        ind = np.argsort(sort_vals)
+    elif sorting == "1-norm": 
+        sort_vals = np.linalg.norm(data, ord=1, axis=1)
+        ind = np.argsort(sort_vals)
+    else:
+        ind = np.lexsort((data[:,1], data[:,0]), axis=0) 
+        
+    lab = 0
+    labels = [-1]*len_ind
+    nr_dist = 0 
+    
+    for i in range(len_ind): # tqdm（range(len_ind), disable=not verbose)
+        sp = ind[i] # starting point
+        if labels[sp] >= 0:
+            continue
+        else:
+            clustc = data[sp,:] 
+            labels[sp] = lab
+            num_group = 1
+
+        for j in ind[i:]:
+            if labels[j] >= 0:
+                continue
+
+            dat = clustc - data[j,:]
+            dist = np.inner(dat, dat)
+            nr_dist += 1
+                
+            if dist <= tol**2:
+                num_group += 1
+                labels[j] = lab
+            else: # apply early stopping
+                if sorting == "2-norm" or sorting == "1-norm":
+                    if (sort_vals[j] - sort_vals[sp] > tol):
+                        break       
+                else:
+                    if ((data[j,0] - data[sp,0] == tol) and (data[j,1] > data[sp,1])) or (data[j,0] - data[sp,0] > tol): 
+                        break
+        splist.append([sp, lab] + [num_group] + list(data[sp,:]) ) # respectively store starting point
+                                                               # index, label, number of neighbor objects, center (starting point).
+        lab += 1
+
+    return np.array(labels), splist, nr_dist 
+
+
+
+def compress(ts, tol=0.5, max_len=np.inf):
+    """
+    Approximate a time series using a continuous piecewise linear function.
+    Parameters
+    ----------
+    ts - numpy ndarray
+        Time series as input of numpy array
+    Returns
+    -------
+    pieces - numpy array
+        Numpy ndarray with three columns, each row contains length, increment, error for the segment.
+    """
+
+    start = 0
+    end = 1
+    pieces = list() # np.empty([0, 3])
+    x = np.arange(0, len(ts))
+    epsilon =  np.finfo(float).eps
+
+    while end < len(ts):
+        inc = ts[end] - ts[start]
+        err = ts[start] + (inc/(end-start))*x[0:end-start+1] - ts[start:end+1]
+        err = np.inner(err, err)
+
+        if (err <= tol*(end-start-1) + epsilon) and (end-start-1 < max_len):
+            (lastinc, lasterr) = (inc, err) 
+            end += 1
+        else:
+            # pieces = np.vstack([pieces, np.array([end-start-1, lastinc, lasterr])])
+            pieces.append([end-start-1, lastinc, lasterr])
+            start = end - 1
+
+    # pieces = np.vstack([pieces, np.array([end-start-1, lastinc, lasterr])])
+    pieces.append([end-start-1, lastinc, lasterr])
+
+    return pieces
+
+
+
+class fABBA:
+    def __init__ (self, tol=0.1, alpha=0.5,  sorting='2-norm', scl=1, verbose=1, max_len=np.inf):
+        """
+        
+        Parameters
+        ----------
+        tol - float
+            Control tolerence for compression, default as 0.1.
+        scl - int
+            Scale for length, default as 1, means 2d-digitization, otherwise implement 1d-digitization.
+        verbose - int
+            Control logs print, default as 1, print logs.
+        max_len - int
+            The max length for each segment, default as np.inf. 
+        
+        """
+        
+        self.tol = tol
+        self.scl = scl
+        self.verbose = verbose
+        self.max_len = max_len
+        self.alpha = alpha
+        self.sorting = sorting
+        self.compress = compress
+        self.compression_rate = None
+        self.digitization_rate = None
+        
+
+    def image_compress(self, data, adjust=False):
+        ts = data.reshape(-1)
+        if adjust:
+            _mean = ts.mean(axis=0)
+            _std = ts.std(axis=0)
+            if _std == 0:
+                _std = 1
+            ts = (ts - _mean) / _std
+            strings = self.fit_transform(ts)
+            self.img_norm = (_mean, _std)
+        else:
+            self.img_norm = None
+            strings = self.fit_transform(ts)
+        return strings, ts[0], self
+
+    
+    def image_decompress(self, strings, start, shape):
+        reconstruction = np.array(self.inverse_transform(strings, start))
+        if self.img_norm != None:
+            reconstruction = reconstruction*self.img_norm[1] + self.img_norm[0]
+        reconstruction = reconstruction.round().reshape(shape).astype(np.uint8)
+        return  reconstruction
+    
+    
+    def fit_transform(self, series):
+        """ 
+        Compress and digitize the time series together.
+        
+        Parameters
+        ----------
+        series - array or list
+            Time series.
+        alpha - float
+            Control tolerence for digitization, default as 0.5.
+        string_form - boolean
+            Whether to return with string form, default as True.
+        """
+        series = np.array(series).astype(np.float64)
+        pieces = np.array(self.compress(ts=series, tol=self.tol, max_len=self.max_len))
+        strings = self.digitize(pieces[:,0:2])
+        self.compression_rate = pieces.shape[0] / series.shape[0]
+        self.digitization_rate = self.centers.shape[0] / pieces.shape[0]
+        if self.verbose in [1, 2]:
+            print("""Compression: Reduced series of length {0} to {1} segments.""".format(series.shape[0], pieces.shape[0]),
+                """Digitization: Reduced {} pieces""".format(len(strings)), "to", self.centers.shape[0], "symbols.")  
+        # strings = ''.join(strings)
+        return strings
+    
+    
+    def digitize(self, pieces, early_stopping=True):
+        """
+        Greedy 2D clustering of pieces (a Nx2 numpy array),
+        using tolernce tol and len/inc scaling parameter scl.
+        In this variant, a 'temporary' cluster center is used 
+        when assigning pieces to clusters. This temporary cluster
+        is the first piece available after appropriate scaling 
+        and sorting of all pieces. It is *not* necessarily the 
+        mean of all pieces in that cluster and hence the final
+        cluster centers, which are just the means, might achieve 
+        a smaller within-cluster tol.
+        """
+        if self.sorting not in ['2-norm', '1-norm', 'lexi']:
+            raise ValueError("Please refer to a specific and correct sorting way, namely '2-norm', '1-norm' and 'lexi'")
+        
+        _std = np.std(pieces, axis=0) # prevent zero-division
+        if _std[0] == 0:
+             _std[0] = 1
+        if _std[1] == 0:
+             _std[1] = 1
+                
+        npieces = pieces * np.array([self.scl, 1]) / _std
+        labels, self.splist, self.nr_dist = aggregate(npieces, self.sorting, self.alpha) # other two variables are used for experiment
+        centers = np.zeros((0,2))
+        for c in range(len(self.splist)):
+            indc = np.argwhere(labels==c)
+            center = np.mean(pieces[indc,:], axis=0)
+            centers = np.r_[ centers, center ]
+        self.centers = centers
+        strings, self.hashmap = self.symbolsAssign(labels)
+        return strings
+            
+            
+
+    
+    def symbolsAssign(self, clusters):
+        """ automatically assign symbols to different clusters, start with '!'
+        Parameters
+        ----------
+        clusters(list or pd.Series or array): the list of clusters.
+        -------------------------------------------------------------
+        Return:
+        symbols(list of string), inverse_hash(dict): repectively for corresponding symbols and hashmap for inverse transform.
+        """
+        
+        clusters = pd.Series(clusters)
+        N = len(clusters.unique())
+
+        cluster_sort = [0] * N 
+        counter = collections.Counter(clusters)
+        for ind, el in enumerate(counter.most_common()):
+            cluster_sort[ind] = el[0]
+
+        alphabet= [chr(i) for i in range(33,33 + N)]
+        hashmap = dict(zip(cluster_sort + alphabet, alphabet + cluster_sort))
+        strings = [hashmap[i] for i in clusters]
+        return strings, hashmap
+
+    
+    def inverse_transform(self, strings, start=0):
+        pieces = self.inverse_digitize(strings, self.centers, self.hashmap)
+        pieces = self.quantize(pieces)
+        time_series = self.inverse_compress(pieces, start)
+        return time_series
+
+    
+    def inverse_digitize(self, strings, centers, hashmap):
+        pieces = np.empty([0,2])
+        for p in strings:
+            pc = centers[int(hashmap[p])]
+            pieces = np.vstack([pieces, pc])
+        return pieces[:,0:2]
+
+    
+    def quantize(self, pieces):
+        if len(pieces) == 1:
+            pieces[0,0] = round(pieces[0,0])
+        else:
+            for p in range(len(pieces)-1):
+                corr = round(pieces[p,0]) - pieces[p,0]
+                pieces[p,0] = round(pieces[p,0] + corr)
+                pieces[p+1,0] = pieces[p+1,0] - corr
+                if pieces[p,0] == 0:
+                    pieces[p,0] = 1
+                    pieces[p+1,0] -= 1
+            pieces[-1,0] = round(pieces[-1,0],0)
+        return pieces
+
+    
+    def inverse_compress(self, pieces, start):
+        """Modified from ABBA package, please see ABBA package to see guidance."""
+        
+        time_series = [start]
+        # stitch linear piece onto last
+        for j in range(0, len(pieces)):
+            x = np.arange(0,pieces[j,0]+1)/(pieces[j,0])*pieces[j,1]
+            #print(x)
+            y = time_series[-1] + x
+            time_series = time_series + y[1:].tolist()
+
+        return time_series
+    
     
     
 class slearn(symbolicML):
@@ -441,13 +737,11 @@ class slearn(symbolicML):
         self.params_secure()
         
         if self.method == 'fABBA':
-            from fABBA import fabba_model
             try:
-                self.s_model = fabba_model(**params, verbose=self.verbose, return_list=True)  
+                self.s_model = fABBA(**params, verbose=self.verbose)  
             except:
                 warnings.warn("Exception, default setting (tol=0.1, alpha=0.1, sorting='2-norm') apply.")
-                self.s_model = fabba_model(tol=0.1, alpha=0.1, sorting='2-norm', verbose=self.verbose,
-                                          return_list=True)
+                self.s_model = fABBA(tol=0.1, alpha=0.1, sorting='2-norm', verbose=self.verbose)
             
             self.string = self.s_model.fit_transform(scale_series)         
             self.last_symbol = self.string[-1] # deprecated symbol, won't take into account
