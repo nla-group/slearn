c45e50864d445a0552b5f775e273ce05dfd90eef
Null <42580530+chenxinye@users.noreply.github.com>
2022-03-10 19:45:07 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import setuptools
 import numpy
 
-with open("README.md", 'r') as f:
+with open("README.rst", 'r') as f:
     long_description = f.read()
 
 setuptools.setup(
     name="slearn",
     packages=["slearn"],
-    version="0.2.2",
+    version="0.2.3",
     setup_requires=["numpy"],
     install_requires=["numpy", "scikit-learn", "pandas", "lightgbm", "requests"],
     include_dirs=[numpy.get_include()],
@@ -30,7 +30,7 @@ setuptools.setup(
                 ],
     maintainer_email="stefan.guettel@manchester.ac.uk",
     description="A package linking symbolic representation with sklearn for time series prediction",
-    long_description_content_type='text/markdown',
+    long_description_content_type='text/x-rst',
     url="https://github.com/nla-group/slearn.git",
     license='MIT License'
 )
